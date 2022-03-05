d25aab2ed7b1e3cb329140a9628552d810db8ab1
tticjswo <tticjswo@naver.com>
2022-03-06 00:19:27 +0900

    [update] detail.html , result.html  수정 // view 수정

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -17,7 +17,7 @@ class Choice(models.Model) :
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
-    def __str(self) :
+    def __str__(self) :
         return self.choice_text
 
 # Create your models here.
